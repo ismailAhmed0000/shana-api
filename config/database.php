a4ac65d07522d2ba85b138fae74d2edca898287e
Ismail Ahmed <143304919+ismailAhmed0000@users.noreply.github.com>
2026-02-03 15:13:24 +0500

    mysql change

--- config/database.php
+++ config/database.php
@@ -3,34 +3,30 @@
 use Illuminate\Support\Str;
 
 return [
+    /*
+     * |--------------------------------------------------------------------------
+     * | Default Database Connection Name
+     * |--------------------------------------------------------------------------
+     * |
+     * | Here you may specify which of the database connections below you wish
+     * | to use as your default connection for database operations. This is
+     * | the connection which will be utilized unless another connection
+     * | is explicitly specified when you execute a query / statement.
+     * |
+     */
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
-    |--------------------------------------------------------------------------
-    | Default Database Connection Name
-    |--------------------------------------------------------------------------
-    |
-    | Here you may specify which of the database connections below you wish
-    | to use as your default connection for database operations. This is
-    | the connection which will be utilized unless another connection
-    | is explicitly specified when you execute a query / statement.
-    |
-    */
-
-    'default' => env('DB_CONNECTION', 'sqlite'),
-
-    /*
-    |--------------------------------------------------------------------------
-    | Database Connections
-    |--------------------------------------------------------------------------
-    |
-    | Below are all of the database connections defined for your application.
-    | An example configuration is provided for each database system which
-    | is supported by Laravel. You're free to add / remove connections.
-    |
-    */
-
+     * |--------------------------------------------------------------------------
+     * | Database Connections
+     * |--------------------------------------------------------------------------
+     * |
+     * | Below are all of the database connections defined for your application.
+     * | An example configuration is provided for each database system which
+     * | is supported by Laravel. You're free to add / remove connections.
+     * |
+     */
     'connections' => [
-
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DB_URL'),
@@ -42,7 +38,6 @@ return [
             'synchronous' => null,
             'transaction_mode' => 'DEFERRED',
         ],
-
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
@@ -62,7 +57,6 @@ return [
                 (PHP_VERSION_ID >= 80500 ? \Pdo\Mysql::ATTR_SSL_CA : \PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
-
         'mariadb' => [
             'driver' => 'mariadb',
             'url' => env('DB_URL'),
@@ -82,7 +76,6 @@ return [
                 (PHP_VERSION_ID >= 80500 ? \Pdo\Mysql::ATTR_SSL_CA : \PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
-
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DB_URL'),
@@ -97,7 +90,6 @@ return [
             'search_path' => 'public',
             'sslmode' => env('DB_SSLMODE', 'prefer'),
         ],
-
         'sqlsrv' => [
             'driver' => 'sqlsrv',
             'url' => env('DB_URL'),
@@ -112,46 +104,40 @@ return [
             // 'encrypt' => env('DB_ENCRYPT', 'yes'),
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
-
     ],
 
     /*
-    |--------------------------------------------------------------------------
-    | Migration Repository Table
-    |--------------------------------------------------------------------------
-    |
-    | This table keeps track of all the migrations that have already run for
-    | your application. Using this information, we can determine which of
-    | the migrations on disk haven't actually been run on the database.
-    |
-    */
-
+     * |--------------------------------------------------------------------------
+     * | Migration Repository Table
+     * |--------------------------------------------------------------------------
+     * |
+     * | This table keeps track of all the migrations that have already run for
+     * | your application. Using this information, we can determine which of
+     * | the migrations on disk haven't actually been run on the database.
+     * |
+     */
     'migrations' => [
         'table' => 'migrations',
         'update_date_on_publish' => true,
     ],
 
     /*
-    |--------------------------------------------------------------------------
-    | Redis Databases
-    |--------------------------------------------------------------------------
-    |
-    | Redis is an open source, fast, and advanced key-value store that also
-    | provides a richer body of commands than a typical key-value system
-    | such as Memcached. You may define your connection settings here.
-    |
-    */
-
+     * |--------------------------------------------------------------------------
+     * | Redis Databases
+     * |--------------------------------------------------------------------------
+     * |
+     * | Redis is an open source, fast, and advanced key-value store that also
+     * | provides a richer body of commands than a typical key-value system
+     * | such as Memcached. You may define your connection settings here.
+     * |
+     */
     'redis' => [
-
         'client' => env('REDIS_CLIENT', 'phpredis'),
-
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')).'-database-'),
+            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')) . '-database-'),
             'persistent' => env('REDIS_PERSISTENT', false),
         ],
-
         'default' => [
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
@@ -164,7 +150,6 @@ return [
             'backoff_base' => env('REDIS_BACKOFF_BASE', 100),
             'backoff_cap' => env('REDIS_BACKOFF_CAP', 1000),
         ],
-
         'cache' => [
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
@@ -177,7 +162,5 @@ return [
             'backoff_base' => env('REDIS_BACKOFF_BASE', 100),
             'backoff_cap' => env('REDIS_BACKOFF_CAP', 1000),
         ],
-
     ],
-
 ];
